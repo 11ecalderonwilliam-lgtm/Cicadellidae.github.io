81ae8a8f2d8d3b313b065fad10c9298962417508
11ecalderonwilliam-lgtm <11ecalderonwilliam@gmail.com>
2025-12-23 22:14:33 -0500

    Fetch and display data from tribus_generos.json

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,1 +1,33 @@
+fetch("data/tribus_generos.json")
+  .then(res => res.json())
+  .then(data => {
+    const tbody = document.querySelector("#tabla-catalogo tbody");
+    let contador = 1;
+
+    for (const tribu in data.tribus) {
+      data.tribus[tribu].forEach(genero => {
+        const nombreArchivo = genero.toLowerCase();
+
+        tbody.innerHTML += `
+          <tr>
+            <td>${contador}</td>
+            <td>
+              <a href="generos/${nombreArchivo}.html">
+                <em>${genero}</em>
+              </a>
+            </td>
+            <td>${tribu}</td>
+            <td>—</td>
+            <td>—</td>
+            <td>
+              <a href="img/${nombreArchivo}.jpg" target="_blank">
+                Ver
+              </a>
+            </td>
+          </tr>
+        `;
+        contador++;
+      });
+    }
+  });
 
