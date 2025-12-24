12166aa4c4411da10fd901401202064f4678fc63
11ecalderonwilliam-lgtm <11ecalderonwilliam@gmail.com>
2025-12-23 22:07:06 -0500

    Add fetch logic for especies.json data display
    Fetch and display data from especies.json in the DOM.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+fetch("data/especies.json")
+  .then(res => res.json())
+  .then(data => {
+    const contenedor = document.getElementById("subfamilias");
+
+    for (const tribu in data.tribus) {
+      contenedor.innerHTML += `<h2>${tribu}</h2><ul></ul>`;
+      const ul = contenedor.querySelector("ul:last-of-type");
+
+      data.tribus[tribu].forEach(genero => {
+        ul.innerHTML += `<li><em>${genero}</em></li>`;
+      });
+    }
+  });
 
